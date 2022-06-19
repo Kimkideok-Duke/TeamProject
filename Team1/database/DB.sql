8fcb8c035953153bad5788d78d029a959a3e2bee
Kimkideok-Duke <65582257+Kimkideok-Duke@users.noreply.github.com>
2022-06-19 23:09:46 +0900

    DB(0619)

--- Team1/database/DB.sql
+++ Team1/database/DB.sql
@@ -1,4 +1,4 @@
-CREATE TABLE user (
+CREATE TABLE users001 (
     userno varchar2(50) PRIMARY KEY,
     id varchar2(50) NOT NULL UNIQUE,
     passwd varchar2(50) NOT NULL,
@@ -11,4 +11,50 @@ CREATE TABLE user (
     interest3 varchar2(50) NOT NULL,
     interest4 varchar2(50) NOT NULL,
     interest5 varchar2(50) NOT NULL
-)
+);
+
+CREATE TABLE notice (
+	noticeno varchar2(50) PRIMARY KEY,
+	nttitle varchar2(500) NOT NULL,
+	ntcontent varchar2(1000) NOT NULL
+);
+
+CREATE TABLE faq (
+	faqno varchar2(50) PRIMARY KEY,
+	question varchar2(500) NOT NULL,
+	answer varchar2(3000) NOT NULL
+);
+
+
+CREATE TABLE review (
+	reviewno varchar2(50) PRIMARY KEY,
+	rvtitle varchar2(500) NOT NULL,
+	rvcontent varchar2(4000) NOT NULL
+);
+
+
+SELECT * FROM users001;
+
+/**   상대 매칭   **/
+SELECT userno, nickname, gender, age, loc, interest1, interest2, interest3, interest4, interest5
+FROM users001
+where gender=?
+AND loc=?
+
+/**   회원 등록   **/
+INSERT INTO users001 values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+
+/**   공지 조회   **/
+SELECT nttitle, ntcontent
+FROM notice
+WHERE noticeno = ?
+
+/**   FAQ 조회   **/
+SELECT question, answer
+FROM faq
+WHERE faqno = ?
+
+/**   리뷰 조회   **/
+SELECT rvtitle, rvcontent
+FROM notice
+WHERE reviewno = ?
